f19b85dd1d9d3a693152df16f35602cb98d334eb
Andrew Ross <andrew.simpson.ross@gmail.com>
2020-01-09 21:43:57 -0600

    folder creation

--- app/server06.js
+++ app/server06.js
@@ -6,7 +6,7 @@ var path = require("path");
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = 3006;
+var PORT = 3007;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
